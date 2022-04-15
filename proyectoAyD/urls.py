6aff3dfab48711a56a6545c01a7cb6fc43013c7e
Julian Florez <julian.1701913963@ucaldas.edu.co>
2022-04-15 12:07:27 -0500

    archivos faltantes

--- proyectoAyD/urls.py
+++ proyectoAyD/urls.py
@@ -13,16 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from re import template
 from django.contrib import admin
 from django.urls import path
 
 from proyectoAyD.views import funcionParametroURL, inicio
 from django.conf.urls import  include
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inicio', inicio),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('parametro/<int:p1>', funcionParametroURL),
     path('', include('grafos.urls'))
 ]
